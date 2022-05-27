2a1afe116d44073fd5bb2391ce9f7a7d4626f046
AliKovala <106349141+AliKovala@users.noreply.github.com>
2022-05-27 17:34:43 +0300

    Update install.py

--- MULTI-MAIL/install.py
+++ MULTI-MAIL/install.py
@@ -2,4 +2,5 @@ import os
 os.system('python -m pip install --upgrade pip')
 os.system('pip install smtplib')
 os.system('pip install colorama')
+os.system('pip install ssl')
 os.system('pip install EmailMessage')
